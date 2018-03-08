e398ce62a1a80e70faf8c347aa95f86897afc777
Vladimir Medolovich <medolovichvova1@gmail.com>
2018-03-08 09:18:18 +0200

    Release

---
+++ index.js
@@ -0,0 +1,104 @@
+"use strict";
+
+const express = require("express");
+const app = express();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const { join } = require("path");
+const PORT = process.env.PORT || 4000;
+
+const rooms = [];
+let turnCount = 0;
+
+app
+    .use('/',express.static(join(__dirname, "public")));
+
+//при подключении игрок попадает в пустую комнату или, если таковой нету, создает новую
+io.on("connection", function(socket) {
+    let found = false;
+    let current;
+    let x = {
+        val: "x",
+        canTurn: true
+    };
+    let o = {
+        val: "o",
+        canTurn: false
+    };
+
+    for(let i = 0, max = rooms.length; i < max; i++) {
+        if(rooms[i].length < 2) {
+            found = true;
+            current = i;
+            console.log(`Room ${current} found.`);
+            break;
+        }
+    }
+
+    if(found === false) {
+        rooms.push([]);
+        current = rooms.length - 1;
+        console.log(`A ${current} room created`);
+    }
+
+    rooms[current].push(socket);
+    socket.emit("connected", current);
+    console.log(`Connected to the ${current} room`);
+
+    if(rooms[current].length < 2) {
+        socket.emit("wait");
+    } else {
+        rooms[current][0].emit("start", x);
+        rooms[current][1].emit("start", o);
+    }
+
+    socket
+        //во время дисконнекта одного из игроков, отправляем информацию об этом оппоненту
+        //если игрок вышел из комнаты будучи там один, комната освобождается
+        .on("disconnect", function() {
+            if(rooms[current].length === 2) {
+                if(rooms[current][0].disconnected) {
+                    rooms[current].shift();
+                } else if(rooms[current][1].disconnected) {
+                    rooms[current].pop();
+                }
+                rooms[current][0].emit("opp disconnected");
+            } else {
+                rooms[current] = [];
+            }
+        })
+        //обработка хода
+        .on("turn", function(data) {
+            turnCount++;
+            if(data.player.val === "x") {
+                o.canTurn = true;
+                x.canTurn = false;
+            } else if(data.player.val === "o") {
+                o.canTurn = false;
+                x.canTurn = true;
+            }
+            rooms[current][0].emit("turn",data,x.canTurn,turnCount);
+            rooms[current][1].emit("turn",data,o.canTurn,turnCount);
+        })
+        //вызывается в момент ничьи
+        .on("draw", function() {
+            turnCount = 0;
+        })
+        //вызывается после проверки на окончание игры
+        .on("end", function(winner) {
+            turnCount = 0;
+            rooms[current].forEach((player) => player.emit("end", winner));
+        })
+        //обработка сообщений
+        .on("new message", (sender,text) => rooms[current].forEach((player) => player.emit("new message", sender, text)))
+        //при вызове, возвращает количество игроков в каждой комнате
+        .on("show rooms", function() {
+            let sendDataRooms = rooms.map((room) => room.map((x) => x.nickname));
+            console.log(sendDataRooms);
+            socket.emit("show rooms", sendDataRooms);
+        })
+        .on("set nickname", (nickname) => socket.nickname = nickname);
+});
+
+http.listen(PORT, () => console.log("Listening on " + PORT));
+//http.listen(PORT, {host: "mvvtictac.herokuapp.com", path: "/"});
--- public/javascripts/tictac.js
+++ public/javascripts/tictac.js
@@ -1,137 +1,153 @@
-(() => {
-    'use strict';
+const tictac = (function TicTacNode() {
+    "use strict";
 
-    const cells = document.querySelectorAll('td');
-    const status = document.getElementById('status');
-    const sendMsg = document.getElementById('message-text');
-    const chat = document.getElementById('chat-window');
-    const roomBtn = document.getElementById('show-rooms-btn');
-    const roomList = document.getElementById('room-list');
+    const cells = document.querySelectorAll("td");
+    const status = document.getElementById("status");
+    const sendMsgBtn = document.getElementById("message-text");
+    const chatWindow = document.getElementById("chat-window");
+    const roomBtn = document.getElementById("show-rooms-btn");
+    const roomList = document.getElementById("room-list");
+    const nicknameWindow = document.getElementById("nickname-setup");
     const socket = io();
 
     let player = {};
-    let started = false; 
-    let shown = false;
-    let clicked = false;
+    let gameStarted = false;
+    let roomBtnShown = false;
+    let roomBtnClicked = false;
 
-    document.addEventListener('keydown', (e) => {
+    document.addEventListener("keydown", function(e) {
         if(e.keyCode === 13) {
+            const nickname = document.getElementById("nickname").value;
+            if(!player.nickname && nickname !== '') {                   
+                nicknameWindow.style.left = "-100%";    
+                player.nickname = nickname;
+                socket.emit("set nickname", player.nickname);
+            }
             sendMessage();
         }
     });
 
-    roomBtn.onclick = () => {
-        if(clicked === false) {
-            socket.emit('show rooms');
-            clicked = true;
+    roomBtn.onclick = function() {
+        if(roomBtnClicked === false) {
+            socket.emit("show rooms");
+            roomBtnClicked = true;
         } else {
-            roomList.style.display = 'none';
-            clicked = false;
+            roomList.style.display = "none";
+            roomBtnClicked = false;
         }
-    }
+    };
 
-    socket
-        .on('connected', roomNum => roomBtn.innerHTML = 'Комната: ' + roomNum)
-        .on('wait', () => {
-            status.innerHTML = 'Ждём второго игрока';
-        })
-        .on('start', pl => {
-            chat.innerHTML = ''; 
-            player = pl;
-            started = true;
-            if(player.canTurn) {
-                status.innerHTML = 'Ваш ход';
-            } else {
-                status.innerHTML = 'Ход оппонента';
-            }
-        })
-        .on('opp-disc', () => {
-            status.innerHTML = 'Ваш оппонент вылетел. Ждём нового.';
-            resetBoard();
-            started = false;
-        })
-        .on('turn', (data,canTurn,currentTurn) => {
-            player.canTurn = canTurn;
-            if(player.canTurn) {
-                status.innerHTML = 'Ваш ход';
-            } else {
-                status.innerHTML = 'Ход оппонента';
-            }
-            cells[data.index].innerHTML = data.player.val.toUpperCase();
-            if(end()) {
-                socket.emit('end',data.player.val);
-            }else if(currentTurn > 8 && !end()) {
-                socket.emit('draw');
-                if(player.canTurn) {
-                    status.innerHTML = 'Ничья. Ваш ход';
-                } else {
-                    status.innerHTML = 'Ничья. Ход оппонента';
-                }
-                resetBoard();
-            }
-        })
-        .on('end', winner =>  {
-            if(player.val === winner) {
-                status.innerHTML = 'С победой! Ход оппонента.';                
-            } else {
-                status.innerHTML = 'Вы проиграли. Ваш ход';
-            }
-            resetBoard();
-        })
-        .on('new message', (sender, text) => {
-            chat.innerHTML += `<div style="padding:5px 10px;background-color: ${sender === player.val ? '#83b6a8' : '#93c5c1'};">${sender}: ${text}</div>`;           
-            chat.scrollTop = chat.scrollHeight;
-        })
-        .on('show rooms', rooms => {
-            roomList.style.display = 'block';
-            if(shown === false) {
-                roomList.innerHTML += '<ul>';
-                for(let i = 0, max = rooms.length; i < max; i++) {
-                    roomList.innerHTML += `<li>Комната ${i}:<ul><li>${rooms[i]}</li></ul></li>`;
-                }
-                roomList.innerHTML += '</ul>';
-                shown = true;
-            }
-        });  
+    roomList.onclick = function() {
+        roomList.style.display = "none";
+    };
 
-    cells.forEach((cell, i) => {
-        cell.onclick = () => {
-            if(started) {
-                if (cell.innerHTML === '') {
-                    if (player.canTurn) {
-                        let data = {
-                            player: player,
-                            index: i
-                        };
-                        socket.emit('turn', data);
-                    }
-                }
+    cells.forEach(function(cell, i) {
+        cell.onclick = function() {
+            if (cell.innerHTML === "" && gameStarted && player.canTurn) {
+                let data = {
+                    player: player,
+                    index: i
+                };
+                socket.emit("turn", data);
             }
-        }
+        };
     });
 
     function sendMessage() {
-        if(sendMsg.value !== '' && started) {
-            socket.emit('new message', player.val, sendMsg.value);
-            sendMsg.value = '';
+        if(sendMsgBtn.value !== "" && gameStarted) {
+            socket.emit("new message", player.nickname, sendMsgBtn.value);
+            sendMsgBtn.value = "";
         }
     }
 
     function resetBoard() {
-        cells.forEach(cell => cell.innerHTML = '');
+        cells.forEach(function(cell) {
+            cell.innerHTML = "";
+        });
     }
 
     function end() {
         return (
-            (cells[0].innerHTML === cells[1].innerHTML && cells[1].innerHTML === cells[2].innerHTML && cells[2].innerHTML !== '')||
-            (cells[3].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[5].innerHTML && cells[5].innerHTML !== '')||
-            (cells[6].innerHTML === cells[7].innerHTML && cells[7].innerHTML === cells[8].innerHTML && cells[8].innerHTML !== '')||
-            (cells[0].innerHTML === cells[3].innerHTML && cells[3].innerHTML === cells[6].innerHTML && cells[6].innerHTML !== '')||
-            (cells[1].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[7].innerHTML && cells[7].innerHTML !== '')||
-            (cells[2].innerHTML === cells[5].innerHTML && cells[5].innerHTML === cells[8].innerHTML && cells[8].innerHTML !== '')||
-            (cells[0].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[8].innerHTML && cells[8].innerHTML !== '')||
-            (cells[2].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[6].innerHTML && cells[6].innerHTML !== '')
+            (cells[0].innerHTML === cells[1].innerHTML && cells[1].innerHTML === cells[2].innerHTML && cells[2].innerHTML !== "")||
+            (cells[3].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[5].innerHTML && cells[5].innerHTML !== "")||
+            (cells[6].innerHTML === cells[7].innerHTML && cells[7].innerHTML === cells[8].innerHTML && cells[8].innerHTML !== "")||
+            (cells[0].innerHTML === cells[3].innerHTML && cells[3].innerHTML === cells[6].innerHTML && cells[6].innerHTML !== "")||
+            (cells[1].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[7].innerHTML && cells[7].innerHTML !== "")||
+            (cells[2].innerHTML === cells[5].innerHTML && cells[5].innerHTML === cells[8].innerHTML && cells[8].innerHTML !== "")||
+            (cells[0].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[8].innerHTML && cells[8].innerHTML !== "")||
+            (cells[2].innerHTML === cells[4].innerHTML && cells[4].innerHTML === cells[6].innerHTML && cells[6].innerHTML !== "")
         );
     }
+
+    socket
+        .on("connected", function(roomNum) {
+            roomBtn.innerHTML = "Комната: " + roomNum;
+        })
+        .on("wait", function() {
+            status.innerHTML = "Ждём второго игрока";
+        })
+        .on("start", function(pl) {
+            chatWindow.innerHTML = "";
+            player = pl;
+            gameStarted = true;
+            if(player.canTurn) {
+                status.innerHTML = "Ваш ход";
+            } else {
+                status.innerHTML = "Ход оппонента";
+            }
+        })
+        .on("opp disconnected", function() {
+            status.innerHTML = "Ваш оппонент вылетел. Ждём нового.";
+            resetBoard();
+            gameStarted = false;
+        })
+        .on("turn", function(data,canTurn,currentTurn) {
+            player.canTurn = canTurn;
+            if(player.canTurn) {
+                status.innerHTML = "Ваш ход";
+            } else {
+                status.innerHTML = "Ход оппонента";
+            }
+            cells[data.index].innerHTML = data.player.val.toUpperCase();
+            if(end()) {
+                socket.emit("end",data.player.val);
+            }else if(currentTurn > 8 && !end()) {
+                socket.emit("draw");
+                if(player.canTurn) {
+                    status.innerHTML = "Ничья. Ваш ход";
+                } else {
+                    status.innerHTML = "Ничья. Ход оппонента";
+                }
+                resetBoard();
+            }
+        })
+        .on("end", function(winner) {
+            if(player.val === winner) {
+                status.innerHTML = "С победой! Ход оппонента.";
+            } else {
+                status.innerHTML = "Вы проиграли. Ваш ход";
+            }
+            resetBoard();
+        })
+        .on("new message", function(sender, text) {
+            chatWindow.innerHTML += `<div style="padding:5px 10px;background-color: ${sender === player.nickname ? "#83b6a8" : "#93c5c1"};">${sender}: ${text}</div>`;
+            chatWindow.scrollTop = chatWindow.scrollHeight;
+        })
+        .on("show rooms", function(rooms) {
+            roomList.style.display = "block";
+            if(roomBtnShown === false) {
+                let inner = "<ul>";
+                rooms.forEach(function(room,i) {
+                    if(room.length > 1) {
+                        inner += `<li id="room-number">Комната ${i}<ul>`;
+                        room.forEach((nickname) => inner += `<li id="player">${nickname !== null ? nickname : "Пользователь не авторизовался"}</li>`);
+                        inner += "</ul></li>";
+                    }
+                });
+                inner += "</ul>";
+
+                roomList.innerHTML = inner;
+            }
+        });
 
 })();
